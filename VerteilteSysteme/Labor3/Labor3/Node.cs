ba944e80431427a7e89c5af744afcdf5e64e2547
Marco Hausadowsky <mhausadowsky@gmx.de>
2017-06-13 14:50:14 +0200

    schweis nimma

--- VerteilteSysteme/Labor3/Labor3/Node.cs
+++ VerteilteSysteme/Labor3/Labor3/Node.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace Labor3
 {
     class Node
     {
+        public UdpClient Socket { get; set; }
+
+
     }
 }
