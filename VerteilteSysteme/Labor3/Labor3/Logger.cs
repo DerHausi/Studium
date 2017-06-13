37fc783c78754d463494fb10f7e2183620a8757b
Marco Hausadowsky <mhausadowsky@gmx.de>
2017-06-13 12:59:04 +0200

    add Logger and Node

---
+++ VerteilteSysteme/Labor3/Labor3/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Labor3
+{
+    class Logger
+    {
+        public UdpClient Socket { get; set; }
+        
+
+
+    }
+}
