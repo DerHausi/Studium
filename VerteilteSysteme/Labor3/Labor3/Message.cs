650efd955888732aecfbee59c5c93a9c96a7d3e6
Marco Hausadowsky <mhausadowsky@gmx.de>
2017-06-13 12:23:32 +0200

    add first changes

---
+++ VerteilteSysteme/Labor3/Labor3/Message.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Labor3
+{
+    public enum MessageType
+    {
+        Info,
+        Echo,
+        Logging
+    }
+
+    [Serializable]
+    public class Message : ISerializable
+    {
+        public MessageType Type { get; set; }
+
+        public uint Number { get; set; }
+
+        public string Data { get; set; }
+
+        private static readonly BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
+        public Message(MessageType type, uint number, string data)
+        {
+            Type = type;
+            Number = number;
+            Data = data;
+        }
+
+        // Constructor to Deserialize
+        public Message(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+
+            Type = (MessageType)info.GetValue($"{nameof(Type)}", typeof(MessageType));
+            Number = (uint)info.GetValue($"{nameof(Number)}", typeof(uint));
+            Data = (string)info.GetValue($"{nameof(Data)}", typeof(string));
+        }
+
+        
+        // Method so that the serializer knows how to serialize
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+
+            info.AddValue($"{nameof(Type)}", Type);
+            info.AddValue($"{nameof(Number)}", Number);
+            info.AddValue($"{nameof(Data)}", Data);
+        }
+
+        public byte[] ToByteArray()
+        {
+            return Message.MessageToByteArray(this);
+        }
+
+        public static byte[] MessageToByteArray(Message message)
+        {
+            byte[] data;
+
+            using (var ms = new MemoryStream())
+            {
+                try
+                {
+                    _binaryFormatter.Serialize(ms, message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Error when parsing to byte array: {e.Message}");
+                    throw;
+                }
+                data = ms.ToArray();
+            }
+
+            return data;
+        }
+
+        public static Message FromByteArray(byte[] byteArray)
+        {
+            using (var ms = new MemoryStream(byteArray))
+            {
+                Message message;
+
+                try
+                {
+                    message = (Message)_binaryFormatter.Deserialize(ms);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Error when parsing byte array to message: {e.Message}");
+                    throw;
+                }
+
+                return message;
+            }
+        }
+    }
+}
